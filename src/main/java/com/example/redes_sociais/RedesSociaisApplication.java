098fa47b4f1dca41b2a30bbf687b42584e18db06
Wesley <wesleyaraujoteixeira98@gmail.com>
2025-03-07 01:46:21 -0300

    enviando dados do projeto

---
+++ src/main/java/com/example/redes_sociais/RedesSociaisApplication.java
@@ -0,0 +1,13 @@
+package com.example.redes_sociais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedesSociaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedesSociaisApplication.class, args);
+	}
+
+}
