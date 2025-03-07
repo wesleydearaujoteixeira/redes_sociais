a71ca934f9a8a8dc5d3680b856f32c5fa6791c7d
Wesley <wesleyaraujoteixeira98@gmail.com>
2025-03-07 02:35:21 -0300

    enviando

--- src/main/java/com/example/redes_sociais/model_comentario/Comentario.java
+++ src/main/java/com/example/redes_sociais/model_comentario/Comentario.java
@@ -32,4 +32,12 @@ public class Comentario {
     private List<Resposta> respostas;
 
     // Getters and Setters
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
 }
